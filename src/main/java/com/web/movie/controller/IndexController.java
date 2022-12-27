bb7da96bdf688e590b810baf773e926b894ecbce
马驹子 <2318332846@qq.com>
2022-12-27 20:39:15 +0800

    fixed 3ad40a3 from https://gitee.com/majuzi/Movie/pulls/5 关于我们

--- src/main/java/com/web/movie/controller/IndexController.java
+++ src/main/java/com/web/movie/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.web.movie.controller;
 import com.web.movie.service.MovieInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -23,5 +24,11 @@ public class IndexController {
     public String toIndex(HttpSession session)throws Exception{
         return "index";
     }
+    @RequestMapping("/about_us")
+    public String toAbout_us(ModelMap map, HttpSession session)throws Exception{
+
+        return "about_us";
+    }
+
 
 }
